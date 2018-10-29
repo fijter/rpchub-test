d642deb8e82d53b574f1ebbc85525ce38e3d908f
Dave de Fijter <dave@indentity.nl>
2018-10-29 23:36:44 +0100

    example folder

---
+++ examples/create_user.py
@@ -0,0 +1,14 @@
+import grpc
+from proto import hub_pb2_grpc
+from proto import messages_pb2
+
+chan = grpc.insecure_channel('localhost:50051')
+stub = hub_pb2_grpc.HubStub(chan)
+
+#This creates a new user and generates a deposit address
+stub.CreateUser(messages_pb2.CreateUserRequest(userId='user-1'))
+
+# Here we get a new Deposit address
+response = stub.GetDepositAddress(messages_pb2.GetDepositAddressRequest(userId='user-1', includeChecksum=True))
+print(response.address)
+
